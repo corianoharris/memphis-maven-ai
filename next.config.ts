915adb341ed61442179b96f243d5531dcb8ed31f
Coriano Harris <corianoharrispro@gmail.com>
2025-11-05 16:52:39 -0600

    updated dep to beasties for inline css

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
   
   // Performance optimizations
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false, // Temporarily disabled to fix build
     optimizePackageImports: ['lucide-react', '@heroicons/react'],
   },
   
